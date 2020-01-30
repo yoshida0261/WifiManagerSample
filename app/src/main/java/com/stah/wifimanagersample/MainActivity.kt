14b50beaf7c226c5dd83d796d53f122f9d642ce2
yoshida0261 <yoshida261@gmail.com>
2020-01-31 01:45:03 +0900

    Initial commit

---
+++ app/src/main/java/com/stah/wifimanagersample/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.stah.wifimanagersample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // wifiチェック
+
+        // offならOnにする。
+
+        // フライトモードなら？
+
+        // dialogを出す
+
+        //
+    }
+}
