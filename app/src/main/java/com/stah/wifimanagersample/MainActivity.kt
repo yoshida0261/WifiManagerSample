793e085fe3dddbb9839f73e44eb5d8e56acea703
yoshida0261 <yoshida261@gmail.com>
2020-01-31 02:25:19 +0900

    wifiの仕様について調べた

--- app/src/main/java/com/stah/wifimanagersample/MainActivity.kt
+++ app/src/main/java/com/stah/wifimanagersample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.stah.wifimanagersample
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,14 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // firebaseから値を取得
+
+
         // wifiチェック
 
         // offならOnにする。
 
-        // フライトモードなら？
+        // フライトモードでも大丈夫だったはず(要確認
 
-        // dialogを出す
+        // だめならdialogを出す
 
-        //
+        // 接続スタート
+
+        // こんな感じでサジェスチョンするらしい。
+        // https://developer.android.com/guide/topics/connectivity/wifi-suggest?hl=ja
+
+        val manager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+
     }
 }
