6b741d7a252596d40016a625e45056a0b27ef678
yoshida0261 <yoshida261@gmail.com>
2020-02-23 10:39:57 +0900

    feat: wifi check and suggest api

--- app/src/main/java/com/stah/wifimanagersample/MainActivity.kt
+++ app/src/main/java/com/stah/wifimanagersample/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.wifi.WifiConfiguration
 import android.net.wifi.WifiManager
 import android.net.wifi.WifiNetworkSuggestion
 import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,80 +19,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // firebaseから値を取得
-
-        // tood
-
-
-        // wifiチェック
-
-        // offならOnにする。
-
-        // フライトモードでも大丈夫だったはず(要確認
-
-        // だめならdialogを出す
-
-        // 接続スタート
-
-        // こんな感じでサジェスチョンするらしい。(android 10 から）
-        // https://developer.android.com/guide/topics/connectivity/wifi-suggest?hl=ja
-
-        // versionごとの切り分けが必要そう￥・・・ひぇ、メンドクサ
-
-        /*
-        val suggestion1 = WifiNetworkSuggestion.
-            .setSsid("test111111")
-            .setIsAppInteractionRequired() // Optional (Needs location permission)
-            .build()
-
-
-         */
-
-
-        // val suggestionsList = listOf(suggestion1)
-
 
         val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         println("wifi ${wifiManager.isWifiEnabled}")
+        if(wifiManager.isWifiEnabled.not()){
+            Snackbar.make(main, "Wi-Fi is disable!!", Snackbar.LENGTH_SHORT).show()
+            return
+        }
 
-        val suggestion1 = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            WifiNetworkSuggestion.Builder()
-                .setSsid("test111111")
-                //.setIsAppInteractionRequired() // Optional (Needs location permission)
-                .build()
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            suggestWifiNetwork(wifiManager)
         } else {
             TODO("VERSION.SDK_INT < Q")
         }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun suggestWifiNetwork(wifiManager:WifiManager){
+        val suggestion1 =
+            WifiNetworkSuggestion.Builder()
+                .setSsid("stah")
+                .setWpa2Passphrase("yoshida100314")
+                //.setIsAppInteractionRequired() // Optional (Needs location permission)
+                .build()
+
 
         val suggestionList = listOf(suggestion1)
-        wifiManager.addNetworkSuggestions(suggestionList)
+        val status = wifiManager.addNetworkSuggestions(suggestionList)
 
-
-        val config = WifiConfiguration()
-        val status = wifiManager.addNetwork(config)
-
-
-        if (status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS) {
-            // do error handling here
-            println(status)
-            wifiManager.setWifiEnabled(true)
-        }
-
-
-// Optional (Wait for post connection broadcast to one of your suggestions)
-        val intentFilter = IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
-
-        val broadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                if (!intent.action.equals(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
-                    return;
-                }
-                // do post connect processing here
-            }
-        };
-        applicationContext.registerReceiver(broadcastReceiver, intentFilter);
-
+        println("addNetworkSuggestion result $status")
 
     }
 }
